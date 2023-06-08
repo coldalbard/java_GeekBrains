8507bf3f926e976a81eda1eefc2b1d7c69d048a4
Coldalbard <coldalbard.coldalbard@gmail.com>
2023-06-08 03:12:00 +0300

    added homework 1 (task 2)

---
+++ homework/homework1/task2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Please enter the number of numbers: ");
+        int size = sc.nextInt();
+        System.out.println(SumPositiveNumbers(size - 1));
+    }
+
+    public static int SumPositiveNumbers(int size) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Please, enter your first number: ");
+        int first_num = sc.nextInt();
+        int countSumPosNumb = 0;
+        while (size != 0) {
+            System.out.printf("Please, enter your next number: ");
+            int number = sc.nextInt();
+            size -= 1;
+            if (number < 0 && first_num > 0) {
+                countSumPosNumb += first_num;
+            }
+            first_num = number;
+        }
+        return countSumPosNumb;
+    }
+}
